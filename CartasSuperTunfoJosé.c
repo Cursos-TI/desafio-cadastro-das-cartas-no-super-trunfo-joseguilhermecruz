74af91a1e4d79045d3d07a4057ad1c2a72e643a8
joseguilhermecruz <guilhermemoraismorais141@gmail.com>
2025-02-11 18:18:48 +0000

    Criei um jogo supertrunfo no modo novato

---
+++ CartasSuperTunfoJosé.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+
+char nome[50];
+float populacao;
+double area;
+double pib;
+int pontos_turisticos;
+
+printf("Digite o nome do país escolhido:\n");
+scanf("%s", &nome);
+printf("Digite o nome do Estado:\n");
+scanf("%s", &nome);
+printf("Digite o nome da Cidade:\n");
+scanf("%s", &nome);
+printf("Digite o codigo da cidade:\n");
+scanf("%s", &nome);
+printf("Insira a População dessa cidade:\n");
+scanf("%f", &populacao);
+printf("Insira a área dessa cidade:\n");
+scanf("%e", &area);
+printf("Insira o PIB dessa Cidade:\n");
+scanf("%e", &pib);
+printf("Insira a quantidade de Pontos turísticos dessa Cidade:\n");
+scanf("%d",&pontos_turisticos);
+
+
+return 0;
+
+
+}
