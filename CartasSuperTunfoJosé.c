7f5cfd6b5bdde6799d23a321e82e664666a5cbdf
joseguilhermecruz <guilhermemoraismorais141@gmail.com>
2025-03-06 16:46:51 +0000

    fiz um jogo super trunfo no nivel aventureiro

--- CartasSuperTunfoJosé.c
+++ CartasSuperTunfoJosé.c
@@ -7,6 +7,11 @@ float populacao;
 double area;
 double pib;
 int pontos_turisticos;
+float densidade_populacional;
+float pib_per_capita;
+
+densidade_populacional = populacao / area;
+pib_per_capita = pib / populacao;
 
 printf("Digite o nome do país escolhido:\n");
 scanf("%s", &nome);
@@ -24,6 +29,8 @@ printf("Insira o PIB dessa Cidade:\n");
 scanf("%e", &pib);
 printf("Insira a quantidade de Pontos turísticos dessa Cidade:\n");
 scanf("%d",&pontos_turisticos);
+printf("Densidade Populacional: %f\n", densidade_populacional);
+printf("PIB Per Capita: %f\n", pib_per_capita);
 
 
 return 0;
