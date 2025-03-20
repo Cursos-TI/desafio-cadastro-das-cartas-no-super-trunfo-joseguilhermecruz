7d9ed410fda6fbec019d348d9d821f4306303e3e
joseguilhermecruz <guilhermemoraismorais141@gmail.com>
2025-03-20 18:18:04 +0000

    fiz um jogo super trunfo no modo Mestre

--- CartasSuperTunfoJosé.c
+++ CartasSuperTunfoJosé.c
@@ -3,7 +3,7 @@
 int main(){
 
 char nome[50];
-float populacaoA;
+unsigned long int populacaoA;
 double areaA;
 double pibA;
 int pontos_turisticosA;
@@ -11,7 +11,7 @@ float densidade_populacionalA;
 float pib_per_capitaA;
 float superpoderA;
 
-float populacaoB;
+unsigned long int populacaoB;
 double areaB;
 double pibB;
 int pontos_turisticosB;
@@ -40,7 +40,7 @@ scanf("%s", &nome);
 printf("Digite o codigo da cidade:\n");
 scanf("%s", &nome);
 printf("Insira a População dessa cidade:\n");
-scanf("%f", &populacaoA);
+scanf("%u", &populacaoA);
 printf("Insira a área dessa cidade:\n");
 scanf("%e", &areaA);
 printf("Insira o PIB dessa Cidade:\n");
@@ -61,7 +61,7 @@ scanf("%s", &nome);
 printf("Digite o codigo da cidade:\n");
 scanf("%s", &nome);
 printf("Insira a População dessa cidade:\n");
-scanf("%f", &populacaoB);
+scanf("%u", &populacaoB);
 printf("Insira a área dessa cidade:\n");
 scanf("%e", &areaB);
 printf("Insira o PIB dessa Cidade:\n");
@@ -72,9 +72,22 @@ printf("Densidade Populacional: %f\n", densidade_populacionalB);
 printf("PIB Per Capita: %f\n", pib_per_capitaB);
 
 
+int resultado_populacao = populacaoA > populacaoB;
+int resultado_area = areaA > areaB;
+int resultado_pib = pibA > pibB;
+int resultado_pontosturisticos = pontos_turisticosA > pontos_turisticosB;
+int resultado_densidade = densidade_populacionalA < densidade_populacionalB;
+int resultado_pibpercapita = pib_per_capitaA > pib_per_capitaB;
+int resultado_superpoder = superpoderA > superpoderB;
 
-printf("")
 
+printf("CartaA tem População maior  que a CartaB %d\n", resultado_populacao);
+printf("CartaA tem Area maior que a CartaB %d\n", resultado_area);
+printf("CartaA tem PIB maior que a CartaB %d\n", resultado_pib);
+printf("CartaA tem mais Pontos Turísticos que a CartaB %d\n", resultado_pontosturisticos);
+printf("CartaA tem uma Densidade Populacional menor que a CartaB %d\n", resultado_densidade);
+printf("CartaA tem PIB Per Capita maior que a CartaB %d\n", resultado_pibpercapita);
+printf("CartaA tem um Super Poder Maior que a CartaB %d\n", resultado_superpoder);
 
 
 
